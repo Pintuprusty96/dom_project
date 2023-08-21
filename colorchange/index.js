ce8d4b61d2955f4c011ddc3b7a7fd42b2f176ee8
Pintuprusty16@gmail.com <pintuprusty16@gmail.com>
2023-08-21 20:24:40 +0530

    change in index.js

--- colorchange/index.js
+++ colorchange/index.js
@@ -6,16 +6,16 @@ but.forEach(function (el) {
     el.addEventListener("click",function(event){
         event.target;
         if(event.target.id=="grey"){
-            body.style.backgroundColor=el.id="grey"
+            body.style.backgroundColor=event.target.id
         }
         if(event.target.id=="white"){
-            body.style.backgroundColor=el.id="white"
+            body.style.backgroundColor=event.target.id
         }
         if(event.target.id=="blue"){
-            body.style.backgroundColor=el.id="blue"
+            body.style.backgroundColor=event.target.id
         }
         if(event.target.id=="yellow"){
-            body.style.backgroundColor=el.id="yellow"
+            body.style.backgroundColor=event.target.id
         }
     })
 })
